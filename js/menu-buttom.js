e781e4616d99b5f8142a555344c155c246483eb1
VorotnikovVitaliy <125999401+VorotnikovVitaliy@users.noreply.github.com>
2023-03-23 18:03:53 +0600

    Delete menu-buttom.js

--- js/menu-buttom.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.header-menu-icon').forEach((el) => {
-    el.addEventListener('click', (e) => {
-
-        
-        e.currentTarget.classList.toggle('open');
-    })
-})
